1a4661c74f9d6449e27f16fc56e0c54cbe15ac31
Eleanor Palmer <elp@sitecore.net>
2019-08-30 08:58:48 -0400

    Code clean up.

--- src/Feature/Team/code/Properties/AssemblyInfo.cs
+++ src/Feature/Team/code/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.HabitatHome.Feature.Components")]
+[assembly: AssemblyTitle("Sitecore.HabitatHome.Feature.Team")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.HabitatHome.Feature.Components")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Sitecore.HabitatHome.Feature.Team")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e2363fa9-faf3-4af6-8a67-07509e0d6e0a")]
+[assembly: Guid("18ed44b8-5775-45ee-969c-a8a255c2e0a1")]
 
 // Version information for an assembly consists of the following four values:
 //
